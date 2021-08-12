0e449541c887c7df52309d2721615c7f70af2343
Jose Gomez <jose.gomez@revature.net>
2021-08-11 18:19:17 -0600

    First Commit for repo

---
+++ Assignment_A/UnitConverter/src/main/Converter.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Converter {
+	public static void main(String[] args) {
+		
+		int menuSelection;
+		
+		while(menuSelection != "4") {
+			System.out.println("Please select an option"
+					+ "\n 1. Cups to Teaspoons"
+					+ "\n 2. Miles to Kilometers"
+					+ "\n 3. US Gallons to Imperial Gallons"
+					+ "\n 4. Quit");
+		}
+		
+	}
+
+}
